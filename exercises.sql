f9643dbbd95d7879468ccb8beee15c9f38538f3e
leoncan122 <leon.can122@gmail.com>
2021-06-26 14:53:12 +0200

    23/06 class exercises

--- exercises.sql
+++ exercises.sql
@@ -12,3 +12,21 @@ SELECT order_id, last_name, ship_name FROM employees e
 JOIN orders o ON e.employee_id = o.employee_id
 WHERE o.employee_id = 4;
 
+--modificaciones
+SELECT * FROM orders WHERE order_id = 10251;
+
+UPDATE orders SET shipped_date = '1996-03-23'
+WHERE order_id = 10250;
+
+ALTER TABLE categories ADD COLUMN fecha_validez DATE;
+
+ALTER TABLE categories DROP COLUMN fecha_validez;
+
+ALTER TABLE categories RENAME COLUMN fecha_validez TO fecha_creacion;
+
+ALTER TABLE categories ALTER COLUMN fecha_creacion TYPE VARCHAR(30);
+
+UPDATE categories SET fecha_creacion = '1994-06-21';
+
+
+SELECT * FROM categories;
